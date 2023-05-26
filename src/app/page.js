e0a3d54c087a39b23b7bf3c130721f8aac135a3a
Kotin Lab <tinhtunaungcu.cu@gmail.com>
2023-05-26 20:51:38 +0630

    Create the news section of the Widgets component

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,12 @@ import Feed from "@/components/Feed";
 import Sidebar from "@/components/Sidebar";
 import Widgets from "@/components/Widgets";
 
-export default function Home() {
+export default async function Home() {
+  // https://saurav.tech/NewsAPI/top-headlines/category/health/in.json
+  const url = 'https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json';
+  const response = await fetch(url);
+  const newsResults = await response.json();
+
   return (
     <main className="flex min-h-screen max-w-7xl mx-auto">
       {/* Sidebar */}
@@ -13,7 +18,7 @@ export default function Home() {
       <Feed />
       
       {/* Widgets */}
-      <Widgets />
+      <Widgets newsResults={newsResults.articles} />
       
       {/* Modal */}
     </main>
--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -1,6 +1,14 @@
+'use client';
+
+import { useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
-export default function Widgets() {
+// Components
+import News from "./News";
+
+export default function Widgets({newsResults}) {
+  const [articleNum, setArticleNum] = useState(3);
+
   return (
     <div className="flex-1 max-w-sm xl:max-w-none px-2 hidden lg:block ml-8 space-y-5">
       <div className="sticky z-40 top-0 py-1.5">
@@ -12,6 +20,21 @@ export default function Widgets() {
             className="absolute pl-11 rounded-full inset-0 border-gray-500 focus:shadow-lg focus:bg-white focus:ring-0 text-gray-700 bg-gray-100"
           />
         </div>
+      </div>
+
+      <div className="text-gray-700 bg-gray-100 rounded-xl pt-4">
+        <h4 className="font-bold text-xl px-4 mb-3">What&apos;s happening?</h4>
+        {newsResults.slice(0, articleNum).map(article => (
+          <News key={article.url} article={article} />
+        ))}
+        {articleNum < newsResults.length && (
+          <button 
+            onClick={() => setArticleNum(articleNum + 3)}
+            className="text-blue-300 hover:text-blue-400 pl-4 pb-3 mt-3"
+          >
+            Show more
+          </button>
+        )}
       </div>
     </div>
   )
